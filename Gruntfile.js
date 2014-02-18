82c39a22518eff956f3683735534b8f6a8f045c7
Boris <bschapira@clever-age.com>
2014-02-18 15:10:00 +0100

    Grunting

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        jshint: {
+            all: ['<%= pkg.name %>.js']
+        },
+
+        uglify: {
+            build: {
+                src: '<%= pkg.name %>.min.js',
+                dest: '<%= pkg.name %>.min.js'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('default', ['jshint', 'uglify']);
+};
